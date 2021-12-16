a486312abfc67e1fa478f3ba79d376cac805bb79
王帝 <di.wang@eoitek.com>
2021-12-16 11:18:32 +0800

    [web] create index from table tb_history

--- owl-web/src/main/resources/db/migration/sqlite/V1.0.0__Baseline.sql
+++ owl-web/src/main/resources/db/migration/sqlite/V1.0.0__Baseline.sql
@@ -46,4 +46,17 @@ CREATE TABLE `tb_history` (
   `integration_name` varchar(255) DEFAULT NULL
 );
 
+CREATE INDEX `idx_history_time` ON tb_history(`time`);
+CREATE INDEX `idx_history_guid` ON tb_history(`guid`);
+CREATE INDEX `idx_history_metric` ON tb_history(`metric`);
+CREATE INDEX `idx_history_instance` ON tb_history(`instance`);
+CREATE INDEX `idx_history_category` ON tb_history(`category`);
+CREATE INDEX `idx_history_host` ON tb_history(`host`);
+CREATE INDEX `idx_history_tag1` ON tb_history(`tag1`);
+CREATE INDEX `idx_history_tag2` ON tb_history(`tag2`);
+CREATE INDEX `idx_history_tag3` ON tb_history(`tag3`);
+CREATE INDEX `idx_history_tag4` ON tb_history(`tag4`);
+CREATE INDEX `idx_history_tag5` ON tb_history(`tag5`);
+CREATE INDEX `idx_history_integration_name` ON tb_history(`integration_name`);
+
 INSERT INTO tb_user (account, password, name, role) VALUES ('admin', '5a8721c145115a3ea48552c43cf8dc64', 'admin', 0);
